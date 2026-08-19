6a12fb400a44dffe5bf4bc1663116b37ebdccb35
Nuhamin <nuhamin.gulilat.7@gmail.com>
2026-08-19 06:48:45 -0400

    added services section

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@
 import Image from "next/image";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
+import services from "@/data/services";
 
 export default function Home() {
   const router = useRouter();
@@ -44,6 +45,32 @@ export default function Home() {
               View Gallery
             </button>
           </div>
+        </div>
+      </section>
+
+      <section>
+        <h2 className="text-5xl font-bold text-center mt-10">What We Do</h2>
+        <p className="text-center text-xl mt-2 text-gray-400 italic font-light">Turning ideas into permanent art.</p>
+
+        <div className="mt-10 grid grid-cols-1 gap-8 px-10 md:grid-cols-2 lg:grid-cols-3">
+          {services.map((service) => (
+            <div key={service.title} className="overflow-hidden rounded-xl shadow-lg">
+              <img
+                src={service.image}
+                alt={service.alt}
+                className="h-56 w-full object-cover"
+              />
+
+              <div className="p-6">
+                <h3 className="text-xl font-semibold">
+                  {service.title}
+                </h3>
+
+                <p className="mt-2 text-gray-600">
+                  {service.description}              </p>
+              </div>
+            </div>
+          ))}
         </div>
       </section>
     </main>
---
+++ data/services.ts
@@ -0,0 +1,40 @@
+const services = [
+    {
+        image: "/images/services/fine-line-tattoo.jpg",
+        title: "Fine Line Tattoos",
+        alt: "Fine line tattoo",
+        description: "Delicate, detailed designs with clean and precise lines.",
+    },
+    {
+        image: "/images/services/black-and-grey-tattoo.jfif",
+        title: "Black & Grey Tattoos",
+        alt: "Black and grey tattoo",
+        description: "Timeless designs crafted with rich shading, depth, and striking contrast.",
+    },
+    {
+        image: "/images/services/color-tattoo.jpg",
+        title: "Color Tattoos",
+        alt: "Color tattoo",
+        description: "Vibrant, bold, and full of life. Custom color work that pops.",
+    },
+    {
+        image: "/images/services/realism-tattoo.jfif",
+        title: "Realism Tattoos",
+        alt: "Realism tattoo",
+        description: "Creating lifelike portraits and scenes with stunning accuracy.",
+    },
+    {
+        image: "/images/services/traditional-tattoo.jfif",
+        title: "Traditional Tattoos",
+        alt: "Traditional tattoo",
+        description: "Bold lines and classic imagery rooted in tattoo heritage.",
+    },
+    {
+        image: "/images/services/cover-up-tattoo.jpg",
+        title: "Cover-Ups & Reworks",
+        alt: "Cover up tattoo",
+        description: "Transforming old or unwanted tattoos into something you’ll love.",
+    }
+]
+
+export default services
