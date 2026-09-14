878b95635919c6f4c6883eb6f0b21b14cec48bc4
Nuhamin <nuhamin.gulilat.7@gmail.com>
2026-09-14 16:35:05 -0400

    added image,title and description on the gallery detail page

--- app/gallery/[id]/page.tsx
+++ app/gallery/[id]/page.tsx
@@ -2,16 +2,26 @@
 
 import { useParams } from "next/navigation"
 import galleries from "@/data/gallery"
+import Image from "next/image"
 
 export default function GalleryDetail() {
     const params = useParams()
     const { id } = params
-    console.log(id)
-    //const gallery = galleries.find((gallery) => gallery.id === id)
+
+    const gallery = galleries.find((gallery) => gallery.id === Number(id))
 
     return (
         <div>
-            <h1>Gallery Detail</h1>
+            <h1 className="text-5xl font-bold text-center mt-10">{gallery?.title}</h1>
+            <div className="flex items-center px-10 justify-center gap-8">
+                <Image
+                    src={gallery?.image || ""}
+                    alt={gallery?.alt || ""}
+                    width={500}
+                    height={500}
+                />
+                <p className="text-center text-xl mt-2 text-gray-400 font-light">{gallery?.description}</p>
+            </div>
         </div>
     )
 }
