e3f224815e7be34fd51d5c0a13097fcc9f54e56b
Nuhamin <nuhamin.gulilat.7@gmail.com>
2026-09-16 17:16:12 -0400

    options for an appointment added

--- app/appointment/page.tsx
+++ app/appointment/page.tsx
@@ -3,7 +3,7 @@ export default function Appointment() {
         <div>
             <h1 className="text-center text-3xl font-bold my-4">Book an Appointment</h1>
             <form>
-                <p>Choose Your Artist</p>
+                <h3>Choose Your Artist</h3>
                 <div className="flex gap-3">
                     <label className="inline-flex items-center justify-center px-4 py-2 rounded-md bg-gray-100 hover:bg-gray-300 cursor-pointer peer-checked:bg-gray-900 peer-checked:text-white" htmlFor="Dereje">
                         <input className="sr-only peer" type="radio" id="Dereje" name="artist" value="Dereje" />
@@ -23,7 +23,25 @@ export default function Appointment() {
                     </label>
                 </div>
                 <div>
-
+                    <h3>Choose your service</h3>
+                    <div className="grid grid-cols-3 gap-3">
+                        <label className="inline-flex items-center justify-center px-4 py-2 rounded-md bg-gray-100 hover:bg-gray-300 cursor-pointer peer-checked:bg-gray-900 peer-checked:text-white" htmlFor="piercing">
+                            <input className="sr-only peer" type="radio" id="consultation" name="service" value="Tattoo Consultation" />
+                            Tattoo Consultation
+                        </label>
+                        <label className="inline-flex items-center justify-center px-4 py-2 rounded-md bg-gray-100 hover:bg-gray-300 cursor-pointer peer-checked:bg-gray-900 peer-checked:text-white" htmlFor="tattoo">
+                            <input className="sr-only peer" type="radio" id="tattoo" name="service" value="Custom Tattoo" />
+                            Custom Tattoo
+                        </label>
+                        <label className="inline-flex items-center justify-center px-4 py-2 rounded-md bg-gray-100 hover:bg-gray-300 cursor-pointer peer-checked:bg-gray-900 peer-checked:text-white" htmlFor="tattoo">
+                            <input className="sr-only peer" type="radio" id="tattoo" name="service" value="Cover Up Tattoo" />
+                            Cover Up Tattoo
+                        </label>
+                        <label className="inline-flex items-center justify-center px-4 py-2 rounded-md bg-gray-100 hover:bg-gray-300 cursor-pointer peer-checked:bg-gray-900 peer-checked:text-white" htmlFor="tattoo">
+                            <input className="sr-only peer" type="radio" id="tattoo" name="service" value="Touch Up Tattoo" />
+                            Touch Up Tattoo
+                        </label>
+                    </div>
                 </div>
             </form>
         </div>
