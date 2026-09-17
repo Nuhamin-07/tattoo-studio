e4657cec54a7914240c478e08ebe202922faaa7d
Nuhamin <nuhamin.gulilat.7@gmail.com>
2026-09-17 14:56:20 -0400

    active navigation menu style added

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,19 +1,36 @@
+"use client"
+
 import Link from 'next/link'
+import { usePathname } from "next/navigation";
 
 export default function Navbar() {
+    const pathname = usePathname();
+
+    const links = [
+        { href: "/", label: "Home" },
+        { href: "/about-us", label: "About Us" },
+        { href: "/contact-us", label: "Contact Us" },
+        { href: "/gallery", label: "Gallery" },
+        { href: "/appointment", label: "Book an Appointment" }
+    ];
     return (
         <nav className="border-b border-gray-200 bg-white">
-            <div className="ml-auto flex max-w-3xl items-center gap-8 px-4 py-4">
-                <Link href="/" className="text-sm font-medium text-gray-700 transition hover:text-black"
-                >Home</Link>
-                <Link href="/about-us" className="text-sm font-medium text-gray-700 transition hover:text-black"
-                >About Us</Link>
-                <Link href="/contact-us" className="text-sm font-medium text-gray-700 transition hover:text-black"
-                >Contact Us</Link>
-                <Link href="/gallery" className="text-sm font-medium text-gray-700 transition hover:text-black"
-                >Gallery</Link>
-                <Link href="/appointment" className="text-sm font-medium text-gray-700 transition hover:text-black"
-                >Book an Appointment</Link>
+            <div className="flex w-full items-center justify-between gap-8 px-12 py-4">
+                <Link href="/"><h3 className="text-xl font-bold text-black transition hover:text-gray-700">Tattoo Studio</h3></Link>
+                <div className="flex gap-8">
+                    {links.map((link) => (
+                        <Link
+                            key={link.href}
+                            href={link.href}
+                            className={`text-sm font-medium transition ${pathname === link.href
+                                ? "text-gray-500"
+                                : "text-black-500 hover:text-black"
+                                }`}
+                        >
+                            {link.label}
+                        </Link>
+                    ))}
+                </div>
             </div>
         </nav>
     )
